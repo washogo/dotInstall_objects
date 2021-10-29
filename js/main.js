b637acbc953035e07f9b3d486d8fa602ad77fd84
washogo <aiz4338150@gmail.com>
2021-10-29 20:06:24 +0900

    [Finish]2/35

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+{
+  const scores = [80, 90, 40];
+
+  
+  // console.log(scores[1]);
+  /*[]で要素のインデックスを指定して呼び出す*/
+
+  // scores[2] = 44;
+  // console.log(scores);
+  /*インデックスを指定して再代入すると値が更新される*/
+
+  // scores = 10;
+  /*定数への直接の再代入はできない
+
+  // console.log(scores.length);
+  /*lengthメソッドは、配列の要素の数を呼び出す命令*/
+}
